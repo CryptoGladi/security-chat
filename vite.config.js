d4cd128b88a22189630d213a80876238476a3237
CryptoGladi <mymamchm@mail.ru>
2023-08-24 20:20:33 +0300

    vite-plugin-imagemin содержит много уязвимостей! Я заменил его на vite-plugin-image-optimizer

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import adapter from '@sveltejs/adapter-static';
-import viteImagemin from 'vite-plugin-imagemin';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		viteImagemin({
-			gifsicle: {
-				optimizationLevel: 7,
-				interlaced: false
-			},
-			optipng: {
-				optimizationLevel: 7
-			},
-			mozjpeg: {
-				quality: 20
-			},
-			pngquant: {
-				quality: [0.8, 0.9],
-				speed: 4
-			},
-			svgo: {
-				plugins: [
-					{
-						name: 'removeViewBox'
-					},
-					{
-						name: 'removeEmptyAttrs',
-						active: false
-					}
-				]
-			}
-		})
+		ViteImageOptimizer({
+		  })
 	],
 
 	kit: {
