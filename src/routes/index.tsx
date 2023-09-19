aab9c8b9e846d22714cd84db9f130a7cf3669d95
CryptoGladi <mymamchm@mail.ru>
2023-09-19 11:11:45 +0300

    registration and components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,31 @@
-import { Title, useNavigate } from "solid-start";
+import { Title, useNavigate } from 'solid-start';
 
 export default function Home() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  return (
-    <main>
-      <Title>Добро пожаловать!</Title>
+	return (
+		<main>
+			<Title>Добро пожаловать!</Title>
 
-      <div class="hero min-h-screen">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-5xl font-bold">Security chat</h1>
-      <p class="py-6">Универсальный чат который даёт вам <strong>приватность</strong>, <strong>анонимность</strong> c открытым исходным кодом</p>
-      <button class="btn btn-primary" onClick={() => {
-        navigate("/login"); // TODO
-      }}>Начать использование!</button>
-    </div>
-  </div>
-</div>
-    </main>
-  );
+			<div class="hero min-h-screen">
+				<div class="hero-content text-center">
+					<div class="max-w-md">
+						<h1 class="text-5xl font-bold">Security chat</h1>
+						<p class="py-6">
+							Универсальный чат который даёт вам <strong>приватность</strong>,{' '}
+							<strong>анонимность</strong> c открытым исходным кодом
+						</p>
+						<button
+							class="btn btn-primary"
+							onClick={() => {
+								navigate('/registration'); // TODO
+							}}
+						>
+							Начать использование!
+						</button>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [ "./src/**/*.{js,jsx,ts,tsx}" ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
 		themes: ['dracula'],
 		logs: process.env.NODE_ENV === 'production' ? false : true
 	}
-} satisfies Config
-
+} satisfies Config;
