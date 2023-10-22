a7d1ed553ee0674715c2f7994a67df74930a6ce3
CryptoGladi <mymamchm@mail.ru>
2023-10-22 11:53:27 +0300

    doing chat

--- src/components/side_bar.tsx
+++ src/components/side_bar.tsx
@@ -28,34 +28,46 @@ const Icon: Component<{ item: Item }> = (props) => {
 	);
 };
 
+function createItemForUser(nickname: string): Item {
+	return new Item(
+		`Пользователь: ${nickname}`,
+		`/main_window/chat/${nickname}`,
+		<FaRegularUser size={24} color="grey" />
+	);
+}
+
 const StantardItems = [
 	new Item('Запросы в друзья', '/main_window/add_friends', <BsPeople size={24} color="grey" />),
 	new Item('Выполнить комманду', '/main_window/run_command', <VsTerminal size={24} color="grey" />)
 ];
 
+const ItemForForEach: Component<{ item: Item }> = (props) => {
+	const navigate = useNavigate();
+
+	return (
+		<div
+			onclick={() => {
+				navigate(props.item.navigate_path);
+			}}
+		>
+			<Icon item={props.item} />
+		</div>
+	);
+};
+
 export const SideBar: Component = () => {
 	const navigate = useNavigate();
+
+	let users = [createItemForUser('Gladi')];
 
 	return (
 		<div class="h-[100svh] border-r-[1px]" style="border-color: grey;">
 			<ul class="menu p-0">
-				<Index each={StantardItems}>
-					{(item, i) => (
-						<div
-							onclick={() => {
-								navigate(item().navigate_path);
-							}}
-						>
-							<Icon item={item()} />
-						</div>
-					)}
-				</Index>
+				<Index each={StantardItems}>{(item, i) => <ItemForForEach item={item()} />}</Index>
 
 				<hr style="border-color: grey;" />
 
-				<Icon
-					item={new Item('Пользователь: Gladi', 'ds', <FaRegularUser size={24} color="grey" />)}
-				/>
+				<Index each={users}>{(item, i) => <ItemForForEach item={item()} />}</Index>
 			</ul>
 		</div>
 	);
--- src/routes/main_window/chat/[nickname].tsx
+++ src/routes/main_window/chat/[nickname].tsx
@@ -2,13 +2,25 @@ import { SideBar } from '~/components/side_bar';
 import { useParams } from 'solid-start';
 
 export default function Index() {
-	const params = useParams();
+	const params = useParams<{ nickname: string }>();
 
 	return (
 		<main class="flex">
 			<SideBar />
 
-			<p>{params.nickname}</p>
+			<div class="h-full w-full">
+				<div class="flex h-10 w-full items-center border-b-[1px] p-2" style="border-color: grey;">
+					<p class="font-bold text-secondary">{params.nickname}</p>
+				</div>
+
+				<div>
+					<p>messages</p>
+				</div>
+
+				<div>
+					<p>messager</p>
+				</div>
+			</div>
 		</main>
 	);
 }
