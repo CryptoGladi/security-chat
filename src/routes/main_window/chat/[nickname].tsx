984a26e9d3f47265287f36050180279ae145b5ea
CryptoGladi <mymamchm@mail.ru>
2023-10-23 21:01:00 +0300

    done flex and message bubble

--- src/routes/main_window/chat/[nickname].tsx
+++ src/routes/main_window/chat/[nickname].tsx
@@ -1,23 +1,45 @@
 import { SideBar } from '~/components/side_bar';
 import { useParams } from 'solid-start';
+import { Component } from 'solid-js';
+
+enum TypeBubble {
+	ChatStart,
+	ChatEnd
+}
+
+const MessageBubble: Component<{ text: string; type: TypeBubble }> = (props) => {
+	return (
+		<div
+			class="chat"
+			classList={{
+				'chat-start': props.type === TypeBubble.ChatStart,
+				'chat-end': props.type === TypeBubble.ChatEnd
+			}}
+		>
+			<div class="chat-bubble">{props.text}</div>
+		</div>
+	);
+};
 
 export default function Index() {
 	const params = useParams<{ nickname: string }>();
 
 	return (
-		<main class="flex">
+		<main class="flex h-screen w-full">
 			<SideBar />
 
-			<div class="h-full w-full">
-				<div class="flex h-10 w-full items-center border-b-[1px] p-2" style="border-color: grey;">
+			<div class="flex h-full w-full flex-col">
+				<div class="w-full flex-none items-center border-b-[1px] p-2" style="border-color: grey;">
 					<p class="font-bold text-secondary">{params.nickname}</p>
 				</div>
 
-				<div>
-					<p>messages</p>
+				<div class="flex-1">
+					<MessageBubble text={'Рандомный текст'} type={TypeBubble.ChatStart} />
+
+					<MessageBubble text={'Ответ на рандомных текст'} type={TypeBubble.ChatEnd} />
 				</div>
 
-				<div>
+				<div class="h-10 flex-none border-t-[1px]" style="border-color: grey;">
 					<p>messager</p>
 				</div>
 			</div>
