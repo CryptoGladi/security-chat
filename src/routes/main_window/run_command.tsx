8374d0e2ac407a0fb6bed1055fdd32fcdcb6aca2
CryptoGladi <mymamchm@mail.ru>
2023-10-27 21:19:55 +0300

    AAAA

--- src/routes/main_window/run_command.tsx
+++ src/routes/main_window/run_command.tsx
@@ -1,10 +1,20 @@
 import { SideBar } from '~/components/side_bar';
+import { autofocus } from '@solid-primitives/autofocus';
+
+autofocus; // prevents from being tree-shaken by TS
 
 export default function Index() {
 	return (
 		<main class="flex">
 			<SideBar />
-			<h1>TODO</h1>
+
+			<div class="flex rounded-lg border border-neutral bg-neutral p-1 shadow-2xl">
+				<input
+					class="input focus:outline-none"
+					placeholder="Выполнить комманду..."
+					autofocus
+				></input>
+			</div>
 		</main>
 	);
 }
